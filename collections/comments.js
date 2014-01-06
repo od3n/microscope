46ebae73e16bc1cd03d4a1dfc7c99ab41958e743
Salahuddin Hairai <mr.od3n@gmail.com>
2014-01-06 23:52:35 +0800

    added basic notifications collection. create a notification when a comment is created

--- collections/comments.js
+++ collections/comments.js
@@ -20,6 +20,15 @@ Meteor.methods({
       submitted: new Date().getTime()
     });
     
-    return Comments.insert(comment);
+    // update the post with the number of comments
+    Posts.update(comment.postId, {$inc: {commentsCount: 1}});
+    
+    // create the comment, save the id
+    comment._id = Comments.insert(comment);
+    
+    // now create a notification, informing the user that there's been a comment
+    createCommentNotification(comment);
+    
+    return comment._id;
   }
 });
